745d9db8366d00bbeb288a2465cd02f57529ebe6
mittu-spidey <mittu.thefire@gmail.com>
2013-09-27 16:40:53 +0530

    database bug fixed.

--- Arkflash/src/com/example/arkflash/ArcflashDataSource.java
+++ Arkflash/src/com/example/arkflash/ArcflashDataSource.java
@@ -8,13 +8,16 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class ArcflashDataSource
 {
+	private String LOG_TAG = "tag";
+
 	private SQLiteDatabase database;
 	private ArcflashSQLiteHelper dbHelper;
 	private String[] allColumns =
-	{ ArcflashSQLiteHelper.RESULT_TITLE, ArcflashSQLiteHelper.LINE_VOLTAGE, ArcflashSQLiteHelper.TRANSFORMER_KVA, ArcflashSQLiteHelper.EQUIPMENT_TYPE, ArcflashSQLiteHelper.TRANSFORMER_Z, ArcflashSQLiteHelper.FAULT_TOLERANCE_TIME, ArcflashSQLiteHelper.GROUNDING, ArcflashSQLiteHelper.INCIDENT_ENERGY, ArcflashSQLiteHelper.EA18, ArcflashSQLiteHelper.EA12 };
+	{ArcflashSQLiteHelper.RESULT_ID, ArcflashSQLiteHelper.RESULT_TITLE, ArcflashSQLiteHelper.LINE_VOLTAGE, ArcflashSQLiteHelper.TRANSFORMER_KVA, ArcflashSQLiteHelper.EQUIPMENT_TYPE, ArcflashSQLiteHelper.TRANSFORMER_Z, ArcflashSQLiteHelper.FAULT_TOLERANCE_TIME, ArcflashSQLiteHelper.GROUNDING, ArcflashSQLiteHelper.INCIDENT_ENERGY, ArcflashSQLiteHelper.EA18, ArcflashSQLiteHelper.EA12 };
 
 	public ArcflashDataSource(Context context)
 	{
@@ -50,12 +53,15 @@ public class ArcflashDataSource
 		values.put(ArcflashSQLiteHelper.EA18, ea18);
 		values.put(ArcflashSQLiteHelper.EA12, ea12);
 
+		Log.i(LOG_TAG, values.toString());
 		long insertId = database.insert(ArcflashSQLiteHelper.TABLE_RESULT, null, values);
+		Log.i(LOG_TAG, "id = " + insertId);
 		Cursor cursor = database.query(ArcflashSQLiteHelper.TABLE_RESULT, allColumns, ArcflashSQLiteHelper.RESULT_ID + " = " + insertId, null, null, null, null);
+		Log.i(LOG_TAG, "cursor object" + cursor.toString());
 		cursor.moveToFirst();
-		arcflashResult newComment = cursorToObject(cursor);
+		arcflashResult newResult = cursorToObject(cursor);
 		cursor.close();
-		return newComment;
+		return newResult;
 	}
 
 	public void deleteResult(arcflashResult result)
@@ -72,9 +78,17 @@ public class ArcflashDataSource
 		Cursor cursor = database.query(ArcflashSQLiteHelper.TABLE_RESULT, allColumns, null, null, null, null, null);
 
 		cursor.moveToFirst();
+		Log.i(LOG_TAG, "cursor object" + cursor.getColumnNames().length);
+		String values[] = cursor.getColumnNames();
+		for (int i = 0; i < values.length; i++)
+		{
+			Log.i(LOG_TAG, "column-" + i + " : " + values[i]);
+		}
+		
 		while (!cursor.isAfterLast())
 		{
 			arcflashResult result = cursorToObject(cursor);
+
 			results.add(result);
 			cursor.moveToNext();
 		}
@@ -86,18 +100,29 @@ public class ArcflashDataSource
 	private arcflashResult cursorToObject(Cursor cursor)
 	{
 		arcflashResult result = new arcflashResult();
-		int i=0;
+		int i = 0;
 		result.setId(cursor.getInt(i++));
+		Log.i(LOG_TAG, "id: " + result.getId());
 		result.setTitle(cursor.getString(i++));
+		Log.i(LOG_TAG, "title : " + result.getTitle());
 		result.setLineVoltage(cursor.getDouble(i++));
+		Log.i(LOG_TAG, "" + result.getLineVoltage());
 		result.setTransformerKva(cursor.getDouble(i++));
+		Log.i(LOG_TAG, "" + result.getTransformerKva());
 		result.setEquipmentType(cursor.getInt(i++));
+		Log.i(LOG_TAG, "" + result.getEquipmentType());
 		result.setTransformerZ(cursor.getDouble(i++));
+		Log.i(LOG_TAG, "" + result.getTransformerZ());
 		result.setFaultToleranceTime(cursor.getDouble(i++));
+		Log.i(LOG_TAG, "" + result.getFaultToleranceTime());
 		result.setGrounding(cursor.getInt(i++));
+		Log.i(LOG_TAG, "" + result.getGrounding());
 		result.setIncidentEnergy(cursor.getDouble(i++));
+		Log.i(LOG_TAG, "" + result.getIncidentEnergy());
 		result.setEa18(cursor.getDouble(i++));
-		result.setEa12(cursor.getDouble(9));
+		Log.i(LOG_TAG, "" + result.getEa18());
+		result.setEa12(cursor.getDouble(i++));
+		Log.i(LOG_TAG, "" + result.getEa12());
 		return result;
 	}
 
